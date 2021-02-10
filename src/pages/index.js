ba4bf687b9f18b37df78f53974a9e2c05ada17d6
Megan Sullivan <megan@gatsbyjs.com>
2021-02-09 16:47:14 -0800

    fix: update home page to use Layout props correctly

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,11 @@
 import * as React from "react"
-import { Link } from 'gatsby'
 import Layout from '../components/layout'
 
 // markup
 const IndexPage = () => {
   return (
-    <Layout title="Home Page">
-      <h1>
-        Congratulations
-        <br />
-        <span>— you just made a Gatsby site! </span>
-        <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
-        </span>
-      </h1>
+    <Layout pageTitle="Home Page">
+      <p>Welcome to my blog!</p>
     </Layout>
   )
 }
