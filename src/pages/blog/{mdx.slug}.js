f4820eaa2c30951e5c101247712c84476b63e576
Megan Sullivan <megan@gatsbyjs.com>
2021-02-22 17:08:44 -0800

    feat: add dynamic hero images to blog post with GatsbyImage

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
+
 import Layout from '../../components/layout'
 
 const BlogPostTemplate = ({data}) => {
+	const image = getImage(data.mdx.frontmatter.hero_image)
+
 	return (
 		<Layout pageTitle={data.mdx.frontmatter.title}>
+			<GatsbyImage 
+				image={image}
+				aria-hidden={true}
+			/>
 			<MDXRenderer>
 				{data.mdx.body}
 			</MDXRenderer>
@@ -20,6 +28,11 @@ export const query = graphql`
 			frontmatter {
 				date(formatString: "MMMM DD, YYYY")
 				title
+				hero_image {
+					childImageSharp {
+						gatsbyImageData
+					}
+				}
 			}
 		}
 	}
